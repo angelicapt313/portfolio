246a937a848a37a78dd125619cf0909c2994cb30
Ag <puchetatome.angelica@gmail.com>
2024-04-21 16:42:01 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+  
+
+  // Obtener referencia al botón de menú de hamburguesa y a la barra de navegación
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const navbar = document.getElementById('navbar');
+  
+  // Agregar un listener para el evento click al botón de menú
+  hamburgerMenu.addEventListener('click', function() {
+    // Alternar la clase "collapsed" en la barra de navegación
+    navbar.classList.toggle('collapsed');
+  });
+  
+  // Añadir un listener para el evento resize de la ventana
+  window.addEventListener('resize', function() {
+    // Si la pantalla es más grande que 768px, asegúrate de que la barra de navegación no esté colapsada
+    if (window.innerWidth > 768) {
+      navbar.classList.remove('collapsed');
+    }
+  });
